52a04644fb15392dd4337193e857b5eab91f28dc
Jose Antonio Leiva Izquierdo <jleivaizq@MacBook-Air.local>
2025-08-24 20:45:53 +0200

    Deativate none-ls

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -7,13 +7,13 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.rubocop,
-				null_ls.builtins.formatting.prettier,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.isort,
-				null_ls.builtins.completion.spell,
-				null_ls.builtins.diagnostics.rubocop,
+				-- null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.rubocop,
+				-- null_ls.builtins.formatting.prettier,
+				-- null_ls.builtins.formatting.black,
+				-- null_ls.builtins.formatting.isort,
+				-- null_ls.builtins.completion.spell,
+				-- null_ls.builtins.diagnostics.rubocop,
 				--null_ls.builtins.diagnostics.eslint_d,
 			},
 		})
